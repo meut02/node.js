f9bdb8e78cc13d5665802b88b91e4788b553e3f0
ut02 <utsavsimplecool@gmail.com>
2023-08-05 22:00:22 +0530

    making server

--- FirstApp.js
+++ FirstApp.js
@@ -1,1 +1,14 @@
-console.log('Hello from Node.js')
+const http=require('http')
+
+const server=http.createServer((req,res)=>{
+    console.log(req.url,req.method,req.headers);
+
+    res.setHeader("Content-Type","text/html");
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from node.js Server</h1></body>');
+    res.write('</html>');
+    res.end();
+})
+
+server.listen(4000);
