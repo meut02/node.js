6645ab09fe9e31d277fc21e6665f125f50ef4ef6
ut02 <utsavsimplecool@gmail.com>
2023-08-06 08:40:21 +0530

    different routes

--- FirstApp.js
+++ FirstApp.js
@@ -1,7 +1,35 @@
 const http=require('http')
+const fs=require('fs')
 
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    const method=req.method
+    if(url==='/home')
+    {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title><head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+       return res.end();   
+    }
+
+    if(url==='/about')
+    {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title><head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+       return res.end();   
+    }
+
+    if(url==='/node')
+    {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title><head>');
+        res.write('<body><h1> Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+       return res.end();   
+    }
 
     res.setHeader("Content-Type","text/html");
     res.write('<html>');
